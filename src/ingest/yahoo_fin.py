65b8aa1aafd224f1a14461de50055a0a012c3335
Dominique Lopes <tech@dlopes.de>
2024-01-22 03:23:27 +0000

    little improvements

--- src/ingest/yahoo_fin.py
+++ src/ingest/yahoo_fin.py
@@ -11,6 +11,8 @@ import pandas as pd
 
 class YahooFinanceDataSource(datasource):
 
+    EMPTY_VALUE = pd.DataFrame(columns=datasource.DATA_FIELDS) 
+
     put_options = None
     call_options = None
 
@@ -24,15 +26,15 @@ class YahooFinanceDataSource(datasource):
     def loadData(self, ticker: str = None, date: date = None):
     
         # reset internal store    
-        self.put_options = pd.DataFrame(columns=datasource.DATA_FIELDS)
-        self.call_options = pd.DataFrame(columns=datasource.DATA_FIELDS)
+        self.put_options = self.EMPTY_VALUE
+        self.call_options = self.EMPTY_VALUE
 
         # get options
         try:
             all_options = opts.get_options_chain(ticker=ticker, date=date, raw=True, headers=types.MappingProxyType({'User-agent': 'Mozilla/5.0'}))
         except ValueError as err:
             self.logger.error(f"unable to retrieve data for symbol {ticker}: {err}")
-            return pd.DataFrame(columns=datasource.DATA_FIELDS)
+            return self.EMPTY_VALUE
 
         # guard: if no data for PUTs is returned then don't update internal store
         if (not all_options['puts'].empty):
@@ -51,7 +53,7 @@ class YahooFinanceDataSource(datasource):
         if (type == datasource.OptionTypes.CALL):
             return self.call_options
             
-        return pd.DataFrame(columns=datasource.DATA_FIELDS)
+        return self.EMPTY_VALUE
     
     
     def getLivePrice(self, ticker):
